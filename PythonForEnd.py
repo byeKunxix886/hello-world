e8965bd26de10ef1dfb9fa99730c830e23c311ac
byekun886 <146324483+byeKunxix886@users.noreply.github.com>
2023-09-29 14:26:22 +0800

    Update PythonForEnd.py

--- PythonForEnd.py
+++ PythonForEnd.py
@@ -4,9 +4,23 @@
 # @File : PythonSyntax
 # @Project : main.py
 
-# 直接在內容裡加空格並不會有影響，要連結字串+' '才會有空格
+# print直接在內容裡加空格並不會有影響，要連結字串+' '才會有空格
 # Python會自動print裡加入\n，所以不要讓print自動換行就將\n拿掉即可
 # print(, end= ' ')將它留白
+
+'''
+for 變量 in 要遍歷的數據:
+ 	內容
+
+有三種方式
+1.range
+(1)range(5) (0,5)0~4 此方法的結果是一個可以遍歷的對象 左閉右開區間
+(2)range(1,3) (起始值,結束值)
+(3)range(1,10,3) (起始值,結束值,步長)
+2.循環一字符串
+3.循環一列表
+*遍歷列表中的下標
+'''
 for i in range(5):
     print('Hi I,m KunCheng. ', end='')
 print()
